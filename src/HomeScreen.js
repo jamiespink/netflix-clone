75add36c370c09b6310dd866549d9cefc268ae21
Jamie Spink <jamiespink2@gmail.com>
2022-07-17 14:40:47 +0200

    dynamic movie rows

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,13 +1,40 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
-import Banner from "./Banner"
+import Banner from "./Banner";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="home-screen">
       <Nav />
       <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   );
 }
